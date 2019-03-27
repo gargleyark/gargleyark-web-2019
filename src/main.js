f41e0bce42803f41e3500d2306e78e5e0c95fef2
gargleyark <myself@gargleyark.com>
2019-03-27 23:46:11 +0000

    adds site description

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
+import DefaultLayout from '~/layouts/Default.vue';
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component('Layout', DefaultLayout);
+  head.meta.push({
+    name: 'description',
+    content:
+      'The website of a Javascript dev who collects books, digs holes, attempts to draw, and then turns it all into an unflattering poem.'
+  });
 }

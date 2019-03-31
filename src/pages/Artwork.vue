2f330314edc0812cfee43097d248295454bdf68c
gargleyark <myself@gargleyark.com>
2019-03-31 21:37:42 +0100

    adds page header to writing and artwork pages

--- src/pages/Artwork.vue
+++ src/pages/Artwork.vue
@@ -1,14 +1,35 @@
 <template>
   <Layout>
-    <h1>art </h1>
-    <p></p>
+    <article class="artwork">
+      <PageHeader :title="pageTitle" />
+    </article>
   </Layout>
 </template>
 
+<page-query>
+  query WritingPage {
+    pageHeader: contentfulPageHeader (id: "2qtqZq4hxOAORjCfAZwngM") {
+      pageTitle
+    }
+  }
+</page-query>
+
 <script>
+import PageHeader from '~/components/PageHeader'
+import SideBlock from '~/components/SideBlock.vue'
+
 export default {
+  components: {
+    PageHeader,
+    SideBlock
+  },
   metaInfo: {
     title: 'Artwork'
+  },
+  computed: { 
+    pageTitle() {
+      return this.$page.pageHeader.pageTitle
+    }
   }
 }
 </script>
--- src/pages/Writing.vue
+++ src/pages/Writing.vue
@@ -1,14 +1,35 @@
 <template>
   <Layout>
-    <h1>write</h1>
-    <p></p>
+    <article class="writing">
+      <PageHeader :title="pageTitle" />
+    </article>
   </Layout>
 </template>
 
+<page-query>
+  query WritingPage {
+    pageHeader: contentfulPageHeader (id: "4IboIMV4KLkNwQDyVMpcqD") {
+      pageTitle
+    }
+  }
+</page-query>
+
 <script>
+import PageHeader from '~/components/PageHeader'
+import SideBlock from '~/components/SideBlock.vue'
+
 export default {
+  components: {
+    PageHeader,
+    SideBlock
+  },
   metaInfo: {
     title: 'Writing'
+  },
+  computed: { 
+    pageTitle() {
+      return this.$page.pageHeader.pageTitle
+    }
   }
 }
 </script>
